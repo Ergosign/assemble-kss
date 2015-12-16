68eef84fe7a6a365b6461ed00741016788ff0e6d
Philipp Schardt <philipp.schardt@ergosign.de>
2015-12-16 11:47:57 +0100

    manual merge with current version from yoman

--- test/comment-parsing_test.js
+++ test/comment-parsing_test.js
@@ -4,13 +4,13 @@ var should = chai.should();
 
 var grunt = require('grunt');
 
-var kssCommentsParser = require ('../lib/kssCommentsParser');
+var kssCommentsParser = require('../lib/kssCommentsParser');
 
-describe("parsing of comments", function() {
+describe("parsing of comments", function () {
 
     var sections;
 
-    beforeEach(function() {
+    beforeEach(function () {
         sections = {
             level: 0
         };
@@ -20,89 +20,91 @@ describe("parsing of comments", function() {
 
         var minimalComment;
 
-        beforeEach(function() {
+        beforeEach(function () {
             minimalComment = {
                 comment: '/* Just a Comment */',
-                srcPath:"doesNotExist/pathIsJustForTesting"
+                srcPath: "doesNotExist/pathIsJustForTesting"
             };
         });
 
         it('should be ignored', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections, grunt);
 
             createdTestSection.should.equal(-1);
         });
     });
 
+
     describe('a minimal comment', function () {
 
         var minimalComment;
 
-        beforeEach(function() {
+        beforeEach(function () {
             minimalComment = {
                 comment: '/*\nTitle \nStyleguide testSection \n*/',
-                srcPath:"doesNotExist/pathIsJustForTesting"
+                srcPath: "doesNotExist/pathIsJustForTesting"
             };
         });
 
         it('should create a section object', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections, grunt);
 
             createdTestSection.should.not.equal(-1);
         });
 
         it('should have a title', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections, grunt);
 
             createdTestSection.should.have.property('sectionTitle').and.equal("Title");
         });
 
         it('should have a sectionName', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections, grunt);
 
             createdTestSection.should.have.property('sectionName').and.equal("testSection");
         });
 
         it('should have a srcPath', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(minimalComment, sections, grunt);
 
             createdTestSection.should.have.property('srcPath').and.equal("doesNotExist/pathIsJustForTesting");
         });
     });
 
+
     describe('a comment with description', function () {
 
         var commentWithDescription;
 
-        beforeEach(function() {
+        beforeEach(function () {
             commentWithDescription = {
                 comment: '/*\nTitle\nA Test Description \nStyleguide testSection \n*/',
-                srcPath:"doesNotExist/pathIsJustForTesting"
+                srcPath: "doesNotExist/pathIsJustForTesting"
             };
         });
 
         it('should have a title', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections, grunt);
 
             createdTestSection.should.have.property('sectionTitle').and.equal("Title");
         });
 
         it('should have a sectionName', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections, grunt);
 
             createdTestSection.should.have.property('sectionName').and.equal("testSection");
         });
 
         it('should have a description', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections, grunt);
 
             createdTestSection.should.have.property('description').and.equal("A Test Description");
         });
@@ -112,18 +114,46 @@ describe("parsing of comments", function() {
 
         var commentWithDescription;
 
-        beforeEach(function() {
+        beforeEach(function () {
             commentWithDescription = {
                 comment: '/*\nTitle\nA Test Description\nDescription Line 2\nLine 3 \nStyleguide testSection \n*/',
-                srcPath:"doesNotExist/pathIsJustForTesting"
+                srcPath: "doesNotExist/pathIsJustForTesting"
             };
         });
 
         it('should have a multi line description', function () {
 
-            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections,grunt);
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections, grunt);
 
             createdTestSection.should.have.property('description').and.equal("A Test Description\nDescription Line 2\nLine 3");
         });
     });
+
+
+    describe('a comment with variatoin', function () {
+
+        var commentWithDescription;
+
+        beforeEach(function () {
+            commentWithDescription = {
+                comment: '/*\nTitle\nA Test Description\nDescription Line 2\nLine 3\nMarkup: test.hbs\n\n.test-class - testdescr \nStyleguide testSection \n*/',
+                srcPath: "doesNotExist/pathIsJustForTesting"
+            };
+        });
+
+        it('should have a markup', function () {
+
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections, grunt);
+
+            createdTestSection.should.have.property('markup').and.equal("test.hbs");
+        });
+
+        it('should have a variation', function () {
+
+            var createdTestSection = kssCommentsParser.getSectionObjectOfKssComment(commentWithDescription, sections, grunt);
+
+            createdTestSection.should.have.property('variations').and.equal([ {variationName: ".test-class", variationDescription: "testdescr" }]);
+        });
+    });
+
 });
